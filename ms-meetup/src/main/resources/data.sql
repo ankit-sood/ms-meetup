acea09d01843e5b8a05b02868691b8759c7a688d
ankit-sood <soodankit1993@gmail.com>
2020-01-25 23:14:26 +0530

    Adding DDL and DML statements for schema.

--- ms-meetup/src/main/resources/data.sql
+++ ms-meetup/src/main/resources/data.sql
@@ -3,3 +3,6 @@ INSERT INTO category VALUES(2,'Sports');
 INSERT INTO category VALUES(3,'Cultural');
 INSERT INTO category VALUES(4,'Wellness');
 INSERT INTO category VALUES(5,'Communication');
+
+INSERT INTO location VALUES(1,'Bangalore','Bellandur Area');
+INSERT INTO location VALUES(2,'Mumbai','Andheri Area');
